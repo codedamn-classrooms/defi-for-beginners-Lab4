72901866cec475726ef2762597a2dea9ba407b81
harshita808 <harshitasaxenah7@gmail.com>
2022-07-19 16:27:34 +0530

    sol file

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,6 @@
 const Loan = artifacts.require("Loan");
 
-module.exports =async function (deployer,network, accounts) {
+module.exports =async function (deployer,accounts) {
   console.log(accounts[1])
-  await deployer.deploy(Loan, 1000, 10, 100, accounts[1], accounts[2]);
+  await deployer.deploy(Loan,1000, 10, 100, accounts[1], accounts[2]);
 };
