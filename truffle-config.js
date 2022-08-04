2c071236df6c3f3c3d13ece6277812e0f437d8f9
harshita <64593306+harshita808@users.noreply.github.com>
2022-08-04 12:12:48 +0530

    update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,20 +1,27 @@
-module.exports = {
-  networks: {
-    development: {
-     host: "127.0.0.1",
-     port: 9545,
-     network_id: "*",
+[11:57 AM, 7/19/2022] Harshita: provide link for tokenswap
+[12:16 PM, 7/19/2022] Harshita: module.exports = {
+  networks: {},
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    useColors: false,
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportFilename: process.env.UNIT_TEST_OUTPUT_FILE + ".json",
+      json: true,
+      html: false,
     },
   },
-  compilers: {
-    solc: {
-      version: "0.8.0",
-      settings: {
-       optimizer: {
-         enabled: false,
-         runs: 200
-       }
-      }
-    }
-  }
+
+  // Configure your compilers
+ compilers: {
+        solc: {
+            version: "native",
+            settings: {
+                optimizer: {
+                    enabled: false
+                }
+            }
+        }
+    },
 };
